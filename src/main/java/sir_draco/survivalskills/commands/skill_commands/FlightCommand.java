828110c7d6113057447c507c88a1b0c5d2d44628
Dylan Buchanan <dylanbuchanan@comcast.net>
2025-07-31 15:23:42 -0400

    Improved flight command logic

--- src/main/java/sir_draco/survivalskills/commands/skill_commands/FlightCommand.java
+++ src/main/java/sir_draco/survivalskills/commands/skill_commands/FlightCommand.java
@@ -13,10 +13,14 @@ import sir_draco.survivalskills.rewards.RewardNotifications;
 import sir_draco.survivalskills.SurvivalSkills;
 
 import java.util.HashMap;
+import java.util.Map;
 
 @SuppressWarnings("NullableProblems")
 public class FlightCommand implements CommandExecutor {
 
+    public static final String BUILDING = "Building";
+    public static final String FLIGHT_I = "FlightI";
+    public static final String FLIGHT_IV = "FlightIV";
     private final SurvivalSkills plugin;
     private final HashMap<Player, FlyingTimer> flyingTimers = new HashMap<>();
 
@@ -31,34 +35,15 @@ public class FlightCommand implements CommandExecutor {
         if (!(sender instanceof Player p)) return false;
 
         // Check if the player can use the command
-        if (!plugin.getSkillManager().getDefaultPlayerRewards().getReward("Building", "FlightI").isEnabled()) {
+        if (!plugin.getSkillManager().getDefaultPlayerRewards().getReward(BUILDING, FLIGHT_I).isEnabled()) {
             p.sendRawMessage(ChatColor.RED + "Flight is not enabled on this server");
             return false;
         }
 
         // Check if the player has a cooldown
         AbilityTimer timer = plugin.getAbilityManager().getAbility(p, "Flight");
-        if (timer != null && !plugin.getSkillManager().getPlayerRewards(p).getReward("Building", "FlightIV").isApplied()) {
-            if (timer.isActive()) {
-                if (strings.length > 0 && strings[0].equalsIgnoreCase("left")) {
-                    p.sendRawMessage(ChatColor.GREEN + "You have " + ChatColor.AQUA
-                            + RewardNotifications.cooldown(timer.getActiveTimeLeft()) + ChatColor.GREEN + " minutes of flight time left!");
-                    p.playSound(p, Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 1, 1);
-                    return true;
-                }
-
-                if (flyingTimers.containsKey(p)) {
-                    flyingTimers.get(p).removeFlight(p);
-                    flyingTimers.remove(p);
-                }
-                timer.endAbility();
-            }
-            else {
-                p.sendRawMessage(ChatColor.RED + "You can use flight again in: " + RewardNotifications.cooldown(timer.getTimeTillReset()));
-                p.playSound(p, Sound.ENTITY_ENDERMAN_TELEPORT, 1, 1);
-                return false;
-            }
-            return true;
+        if (timer != null && !plugin.getSkillManager().getPlayerRewards(p).getReward(BUILDING, FLIGHT_IV).isApplied()) {
+            return checkForActiveTimer(strings, p, timer);
         }
         else if (p.getAllowFlight()) {
             p.setAllowFlight(false);
@@ -73,28 +58,22 @@ public class FlightCommand implements CommandExecutor {
         int activeTime;
         int speed;
         float baseSpeed = 0.025f;
-        if (!plugin.getSkillManager().getPlayerRewards(p).getReward("Building", "FlightI").isEnabled()
-            || !plugin.getSkillManager().getPlayerRewards(p).getReward("Building", "FlightI").isApplied()) {
-            p.sendRawMessage(ChatColor.RED + "You need to be building level " + ChatColor.AQUA
-                    + plugin.getSkillManager().getDefaultPlayerRewards().getReward("Building", "FlightI").getLevel()
-                    + ChatColor.RED + " to use Flight");
-            p.playSound(p, Sound.ENTITY_ENDERMAN_TELEPORT, 1, 1);
-            return true;
-        }
-        else if (!plugin.getSkillManager().getPlayerRewards(p).getReward("Building", "FlightII").isEnabled()
-                || !plugin.getSkillManager().getPlayerRewards(p).getReward("Building", "FlightII").isApplied()) {
+        if (canUseFlight(p)) return true;
+
+        if (!plugin.getSkillManager().getPlayerRewards(p).getReward(BUILDING, "FlightII").isEnabled()
+                || !plugin.getSkillManager().getPlayerRewards(p).getReward(BUILDING, "FlightII").isApplied()) {
             resetTime = 3600; // 60 minutes
             activeTime = 300; // 5 minutes
             speed = 1;
         }
-        else if (!plugin.getSkillManager().getPlayerRewards(p).getReward("Building", "FlightIII").isEnabled() ||
-                !plugin.getSkillManager().getPlayerRewards(p).getReward("Building", "FlightIII").isApplied()) {
+        else if (!plugin.getSkillManager().getPlayerRewards(p).getReward(BUILDING, "FlightIII").isEnabled() ||
+                !plugin.getSkillManager().getPlayerRewards(p).getReward(BUILDING, "FlightIII").isApplied()) {
             resetTime = 1800; // 30 minutes
             activeTime = 900; // 15 minutes
             speed = 2;
         }
-        else if (!plugin.getSkillManager().getPlayerRewards(p).getReward("Building", "FlightIV").isEnabled() ||
-                !plugin.getSkillManager().getPlayerRewards(p).getReward("Building", "FlightIV").isApplied()){
+        else if (!plugin.getSkillManager().getPlayerRewards(p).getReward(BUILDING, FLIGHT_IV).isEnabled() ||
+                !plugin.getSkillManager().getPlayerRewards(p).getReward(BUILDING, FLIGHT_IV).isApplied()){
             resetTime = 1800; // 30 minutes
             activeTime = 1800; // 30 minutes
             speed = 3;
@@ -109,6 +88,11 @@ public class FlightCommand implements CommandExecutor {
             return true;
         }
 
+        startFlightTimer(p, baseSpeed, speed, activeTime, resetTime);
+        return true;
+    }
+
+    private void startFlightTimer(Player p, float baseSpeed, int speed, int activeTime, int resetTime) {
         p.setAllowFlight(true);
         p.setFlying(true);
         p.setFlySpeed(baseSpeed * speed);
@@ -122,10 +106,48 @@ public class FlightCommand implements CommandExecutor {
         p.sendRawMessage(ChatColor.GREEN + "You have enabled your flight for " + ChatColor.AQUA
                 + (activeTime / 60) + ChatColor.GREEN + " minutes!");
         p.playSound(p, Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 1, 1);
+    }
+
+    private boolean canUseFlight(Player p) {
+        if (!plugin.getSkillManager().getPlayerRewards(p).getReward(BUILDING, FLIGHT_I).isEnabled()
+            || !plugin.getSkillManager().getPlayerRewards(p).getReward(BUILDING, FLIGHT_I).isApplied()) {
+            p.sendRawMessage(ChatColor.RED + "You need to be building level " + ChatColor.AQUA
+                    + plugin.getSkillManager().getDefaultPlayerRewards().getReward(BUILDING, FLIGHT_I).getLevel()
+                    + ChatColor.RED + " to use Flight");
+            p.playSound(p, Sound.ENTITY_ENDERMAN_TELEPORT, 1, 1);
+            return true;
+        }
+        return false;
+    }
+
+    private boolean checkForActiveTimer(String[] strings, Player p, AbilityTimer timer) {
+        if (timer.isActive()) {
+            if (strings.length > 0 && strings[0].equalsIgnoreCase("left")) {
+                p.sendRawMessage(ChatColor.GREEN + "You have " + ChatColor.AQUA
+                        + RewardNotifications.cooldown(timer.getActiveTimeLeft()) + ChatColor.GREEN + " minutes of flight time left!");
+                p.playSound(p, Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 1, 1);
+                return true;
+            }
+
+            // Inform the player flight is being deactivated
+            p.sendRawMessage(ChatColor.YELLOW + "You have deactivated your flight!");
+            p.playSound(p, Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 1, 1);
+
+            if (flyingTimers.containsKey(p)) {
+                flyingTimers.get(p).removeFlight(p);
+                flyingTimers.remove(p);
+            }
+            timer.endAbility();
+        }
+        else {
+            p.sendRawMessage(ChatColor.RED + "You can use flight again in: " + RewardNotifications.cooldown(timer.getTimeTillReset()));
+            p.playSound(p, Sound.ENTITY_ENDERMAN_TELEPORT, 1, 1);
+            return false;
+        }
         return true;
     }
 
-    public HashMap<Player, FlyingTimer> getFlyingTimers() {
+    public Map<Player, FlyingTimer> getFlyingTimers() {
         return flyingTimers;
     }
 }
